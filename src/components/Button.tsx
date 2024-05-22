a084b29e74dc0d96286fc0efa51daba5435cb57b
Nikhil Kumar <nnnnikhil6@gmail.com>
2024-05-22 23:27:51 +0530

    adding new npm

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type PropsType = {
+  children: React.ReactNode;
+};
+
+const Button = ({children}: PropsType) => {
+  return (
+    <button
+      style={{
+        padding: "10px 20px",
+        fontSize: "1.2em",
+        borderRadius: "5px",
+        cursor: "pointer",
+        backgroundColor: "blue",
+        color: "white",
+        border: "none",
+      }}>
+      {children}
+    </button>
+  );
+}
+
+export { Button };
